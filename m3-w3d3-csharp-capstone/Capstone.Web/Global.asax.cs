b43e795c3f647f1d064a0b8687f18920ef23c1bd
Drew Woitovich <drew.woitovich@live.com>
2018-07-18 13:43:49 -0400

    adds Bootstrap and jQuery, fixes missing web.config, fixes messed up packages

--- m3-w3d3-csharp-capstone/Capstone.Web/Global.asax.cs
+++ m3-w3d3-csharp-capstone/Capstone.Web/Global.asax.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Optimization;
 using Capstone.Web.App_Start;
 
 namespace Capstone.Web
@@ -16,7 +15,7 @@ namespace Capstone.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
         }
     }
 }
